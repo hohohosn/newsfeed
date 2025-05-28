6e7d1a276c78cf9508416f8d7435650fa05be173
kodongwon <dw9706@gmail.com>
2025-05-28 14:39:33 +0900

    feat: UserRepository 임시 생성

--- src/main/java/com/example/newsfeed/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/newsfeed/domain/user/repository/UserRepository.java
@@ -1,7 +1,27 @@
 package com.example.newsfeed.domain.user.repository;
 
 import com.example.newsfeed.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
+
+@Repository
+public class UserRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(User user) {
+        em.persist(user);
+    }
+
+    public Optional<User> findById(Long id) {
+        return Optional.ofNullable(em.find(User.class, id));
+    }
+
+    public void remove(User user) {
+        em.remove(user);
+
+    }
 }
