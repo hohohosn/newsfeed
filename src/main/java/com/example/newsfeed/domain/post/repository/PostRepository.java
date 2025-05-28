20f7ca6489c34bd4ac2e67275d78239ff8d539aa
hohohosn <day7of01@gmail.com>
2025-05-28 20:25:12 +0900

    레포지터리 인터페이스

--- src/main/java/com/example/newsfeed/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/newsfeed/domain/post/repository/PostRepository.java
@@ -1,6 +1,15 @@
 package com.example.newsfeed.domain.post.repository;
 
+import com.example.newsfeed.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
 
-public class PostRepository{
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    default Post findByIdOrElseThrow(Long postId) {
+        return findById(postId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + postId));
+    }
 }
+
