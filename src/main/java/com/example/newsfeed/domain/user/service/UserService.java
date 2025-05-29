8ad40a9667ea8370d5047099d071b8eec6049535
kodongwon <dw9706@gmail.com>
2025-05-29 16:04:38 +0900

    fix: 친구 추가 시 중복 체크, 친구 삭제 시 존재 확인 로직 추가
    친구 추가 시 중복 데이터 생기는 상황울  제거하기 위해 이미 친구 추가가 되어있는지 확인.

--- src/main/java/com/example/newsfeed/domain/user/service/UserService.java
+++ src/main/java/com/example/newsfeed/domain/user/service/UserService.java
@@ -16,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -26,6 +27,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final FriendshipRepository friendshipRepository;
+
     //회원가입 signup
     @Transactional
     public Long signup(UserRequestDto dto) {
@@ -34,12 +36,15 @@ public class UserService {
         //비밀번호 암호화
         user.setEncodedPassword(passwordEncoder.encode(dto.getPassword()));
 
-        userRepository.findByEmail(dto.getEmail()).ifPresent(u -> {throw new IllegalArgumentException("이메일이 이미 존재합니다.");});
+        userRepository.findByEmail(dto.getEmail()).ifPresent(u -> {
+            throw new IllegalArgumentException("이메일이 이미 존재합니다.");
+        });
 
         userRepository.save(user);
 
         return user.getId();
     }
+
     // 회원탈퇴
     @Transactional
     public void withdrawal(Long id) {
@@ -68,6 +73,7 @@ public class UserService {
         user.updateProfile(dto.getUserName(), dto.getPhoneNumber(), dto.getBirth());
         return UserMapper.toResponseDto(user);
     }
+
     @Transactional
     public void updatePassword(Long id, String oldPassword, String newPassword) {
         User user = findByIdOrElseThrow(id);
@@ -82,15 +88,19 @@ public class UserService {
     public void addFriend(Long id, Long friendId) {
         User user = findByIdOrElseThrow(id);
         User friend = findByIdOrElseThrow(friendId);
-        friendshipRepository.save(new Friendship(user, friend));
+        //TODO 예외 던지기, if구문으로 검증 후 패스 어떤거 선택??
+        if (!friendshipRepository.existsByUserAndFriend(user, friend)) {
+            friendshipRepository.save(new Friendship(user, friend));
+        }
     }
 
     @Transactional
     public void deleteFriend(Long id, Long friendId) {
         User user = findByIdOrElseThrow(id);
         User friend = findByIdOrElseThrow(friendId);
-        Friendship friendship = friendshipRepository.findByUserAndFriend(user, friend).orElseThrow(() -> new EntityNotFoundException("유저(id:" + user.getId() + ")와 유저(id:" + friend.getId() + ")의 친구 관계가 없습니다."));
-        friendshipRepository.delete(friendship);
+        // TODO 예외 던지기, 검증 후 패스 어떤거 선택??
+//       Friendship friendship = friendshipRepository.findByUserAndFriend(user, friend).orElseThrow(() -> new EntityNotFoundException("유저(id:" + user.getId() + ")와 유저(id:" + friend.getId() + ")의 친구 관계가 아닙니다."));
+        friendshipRepository.findByUserAndFriend(user, friend).ifPresent(friendshipRepository::delete);
     }
 
     private User findByIdOrElseThrow(Long id) {
