6281f013784fa828ae711b523efa8a405b81e8e8
DongGeun <kk506kk@naver.com>
2025-05-30 15:29:23 +0900

    user Bean Validation

--- src/main/java/com/example/newsfeed/domain/user/dto/UserRequestDto.java
+++ src/main/java/com/example/newsfeed/domain/user/dto/UserRequestDto.java
@@ -1,6 +1,8 @@
 package com.example.newsfeed.domain.user.dto;
 
-import com.example.newsfeed.domain.user.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +15,17 @@ import java.time.LocalDate;
 @NoArgsConstructor
 
 public class UserRequestDto {
+    @Email(message = "올바른 형식의 이메일 주소를 입력해주세요")
     private String email;
+
     private String password;
+
+    @Pattern(regexp = "^\\d{10,11}$", message = "전화번호 숫자는 10~11자리입니다.")
     private String phoneNumber;
+
     private String userName;
+
+    @PastOrPresent(message = "[과거 또는 현재의 날짜여야 합니다")
     private LocalDate birth;
 
 
